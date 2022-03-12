3a763d25de53781be9a4010bdadfb941ffb8b161
R3M1d4Cod3r <superchristian9@gmail.com>
2022-03-12 14:29:16 +0100

    commit

--- Lab01/lab01.js
+++ Lab01/lab01.js
@@ -13,14 +13,47 @@ function reduce_string( arr ){
 }
 
 
-console.log(reduce_string(["it"]));
+//console.log(reduce_string(["it"]));
 
 // Ex1
+const dayjs = require('dayjs');
 
-function Film(id,title,favorites=false,date,rating){
+function Film(id,title,favorites=false,date=undefined,rating){
     this.id = id;
     this.title = title;
     this.favorites = favorites;
-    this.date = date;
+    this.date = (date!=undefined) ? dayjs(date,'YYYY-MM-DD'):undefined;
     this.rating = rating;
+    this.str = () => { return `${this.id} ${this.title} ${this.favorites} ${(date!=undefined) ? this.date.format('DD/MM/YYYY'):undefined} ${this.rating}`;}
 }
+
+function FilmList(){
+    this.list =  [];
+    this.addNewFilm = (e) => {
+        this.list.push(e);
+    }
+    this.sortByDate = (e) => {
+        return this.list.sort( (a,b)=>{
+            if(a.date==undefined)
+                return 1;
+            if(b.date==undefined)
+                return -1;
+
+            return a.date.diff(b.date);
+        });
+    this.deleteFilm = (e) => {
+        
+    }
+    }
+    
+}
+let films= new  FilmList();
+films.addNewFilm( new Film(1,"Pulp Fiction", true, "2022-03-10", 5));
+films.addNewFilm( new Film(2,"21 Grams", true, "2022-03-17", 4));
+films.addNewFilm( new Film(3,"Star Wars", false));
+films.addNewFilm( new Film(4,"Matrix", false));
+films.addNewFilm( new Film(5,"Shrek", false, "2022-03-21", 3));
+films.list.forEach(e => console.log(e.str()));
+console.log("----------------")
+let sorted = films.sortByDate()
+sorted.forEach(e => console.log(e.str()));
