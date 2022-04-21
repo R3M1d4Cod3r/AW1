2fc26c05ad30812febcd211dce2e1f0b8188a7b9
R3M1d4Cod3r <superchristian9@gmail.com>
2022-04-21 11:22:05 +0200

    commit

--- Lab04/script.js
+++ Lab04/script.js
@@ -43,7 +43,7 @@ function FilmList() {
 
 
 let films = new FilmList();
-films.addNewFilm(new Film(1, 'Pulp Fiction', true, '2022-03-10', 2));
+films.addNewFilm(new Film(1, 'Pulp Fiction', true, '2022-03-10', 5));
 films.addNewFilm(new Film(2, '21 Grams', true, '2022-03-17', 4));
 films.addNewFilm(new Film(3, 'Star Wars', false));
 films.addNewFilm(new Film(4, 'Matrix', false));
